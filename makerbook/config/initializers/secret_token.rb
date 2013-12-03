e91b84e576fd685220c20c2a213e1e1c7fa8e818
John Caldwell <caldwelljs@gmail.com>
2013-12-03 16:13:51 -0600

    first commit

---
+++ makerbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makerbook::Application.config.secret_key_base = '98bb4a116dec5ac7d33d082ba7fc6fe84ef334100db5c4e8d9dfc1483308fe555ca8e97b42b2470d1211a128665a6f9e552b2e7ac590d21a90b4b62f13d1f414'
